c075f24ca26045ac58fa175c0f3325da827d3770
tera8 <82479019+tera8m4@users.noreply.github.com>
2024-06-30 21:46:04 +0300

    Added utf8 support for parser

--- budoux/source/budoux.cpp
+++ budoux/source/budoux.cpp
@@ -1,8 +1,63 @@
 #include "budoux.h"
 #include "models/model.h"
+#include <list>
 #include <numeric>
 #include <string_view>
+#include <utf8.h>
 #include <vector>
+
+namespace ParserHelpers {
+class IteratorCache final {
+    static constexpr int32_t CACHE_SIZE = 7;
+
+public:
+    IteratorCache(const std::string_view& InSentence)
+        : Sentence{InSentence}
+        , Iterators{CACHE_SIZE, InSentence.cend()} {
+
+        utf8::iterator it(Sentence.begin(), Sentence.begin(), Sentence.end());
+
+        for (int32_t i = 2; i < CACHE_SIZE; ++i) {
+            Iterators[i] = it.base();
+
+            try {
+                ++it;
+            } catch (const std::exception& e) {
+                break;
+            }
+        }
+    }
+
+    const std::string_view::const_iterator& GetIterator(const int32_t InIndex) const {
+        return Iterators[(Offset + CACHE_SIZE + InIndex) % CACHE_SIZE];
+    }
+
+    const std::string_view::const_iterator& operator[](const int32_t InIndex) const {
+        return GetIterator(InIndex);
+    }
+
+    std::string_view CreateView(const int32_t InStart, const int32_t InEnd) const {
+        return std::string_view{GetIterator(InStart), GetIterator(InEnd)};
+    }
+
+    void ReadNext() {
+        const auto& LastIterator = GetIterator(CACHE_SIZE - 1);
+        utf8::iterator it(LastIterator, LastIterator, Sentence.end());
+        try {
+            ++it;
+            Iterators[Offset] = it.base();
+        } catch (const std::exception& e) {
+            Iterators[Offset] = Sentence.end();
+        }
+        Offset = (Offset + 1) % CACHE_SIZE;
+    }
+
+private:
+    std::string_view Sentence;
+    std::vector<std::string_view::const_iterator> Iterators;
+    int32_t Offset = 0;
+};
+} // namespace ParserHelpers
 
 namespace budoux {
 Parser::Parser(const Model& InModel)
@@ -23,51 +78,59 @@ int Parser::ComputeBaseScore() const noexcept {
 std::vector<std::string_view> Parser::Parse(const std::string_view Sentence) const {
     std::vector<std::string_view> result;
 
-    int lastBound = 0;
-    for (int i = 1; i < Sentence.size(); ++i) {
+    std::string_view::const_iterator lastBound = Sentence.cbegin();
+
+    const int32_t letters = utf8::distance(Sentence.begin(), Sentence.end());
+    ParserHelpers::IteratorCache Cache{Sentence};
+
+    for (int i = 1; i < letters; ++i) {
         int score = BaseScore;
 
         if (i - 2 > 0) {
-            score += ParsingModel.GetScore(Model::EParams::UW1, Sentence.substr(i - 3, 1));
+            score += ParsingModel.GetScore(Model::EParams::UW1, Cache.CreateView(0, 1));
         }
         if (i - 1 > 0) {
-            score += ParsingModel.GetScore(Model::EParams::UW2, Sentence.substr(i - 2, 1));
+            score += ParsingModel.GetScore(Model::EParams::UW2, Cache.CreateView(1, 2));
         }
-        score += ParsingModel.GetScore(Model::EParams::UW3, Sentence.substr(i - 1, 1));
-        score += ParsingModel.GetScore(Model::EParams::UW4, Sentence.substr(i, 1));
-        if (i + 1 < Sentence.size()) {
-            score += ParsingModel.GetScore(Model::EParams::UW5, Sentence.substr(i + 1, 1));
+        score += ParsingModel.GetScore(Model::EParams::UW3, Cache.CreateView(2, 3));
+        score += ParsingModel.GetScore(Model::EParams::UW4, Cache.CreateView(3, 4));
+        if (i + 1 < letters) {
+            score += ParsingModel.GetScore(Model::EParams::UW5, Cache.CreateView(4, 5));
         }
-        if (i + 2 < Sentence.size()) {
-            score += ParsingModel.GetScore(Model::EParams::UW6, Sentence.substr(i + 2, 1));
+        if (i + 2 < letters) {
+            score += ParsingModel.GetScore(Model::EParams::UW6, Cache.CreateView(5, 6));
         }
         if (i > 1) {
-            score += ParsingModel.GetScore(Model::EParams::BW1, Sentence.substr(i - 2, 2));
+            score += ParsingModel.GetScore(Model::EParams::BW1, Cache.CreateView(1, 3));
         }
-        score += ParsingModel.GetScore(Model::EParams::BW2, Sentence.substr(i - 1, 2));
-        if (i + 1 < Sentence.size()) {
-            score += ParsingModel.GetScore(Model::EParams::BW3, Sentence.substr(i, 2));
+        score += ParsingModel.GetScore(Model::EParams::BW2, Cache.CreateView(2, 4));
+        if (i + 1 < letters) {
+            score += ParsingModel.GetScore(Model::EParams::BW3, Cache.CreateView(3, 5));
         }
         if (i - 2 > 0) {
-            score += ParsingModel.GetScore(Model::EParams::TW1, Sentence.substr(i - 3, 3));
+            score += ParsingModel.GetScore(Model::EParams::TW1, Cache.CreateView(0, 3));
         }
         if (i - 1 > 0) {
-            score += ParsingModel.GetScore(Model::EParams::TW2, Sentence.substr(i - 2, 3));
+            score += ParsingModel.GetScore(Model::EParams::TW2, Cache.CreateView(1, 4));
         }
-        if (i + 1 < Sentence.size()) {
-            score += ParsingModel.GetScore(Model::EParams::TW3, Sentence.substr(i - 1, 3));
+        if (i + 1 < letters) {
+            score += ParsingModel.GetScore(Model::EParams::TW3, Cache.CreateView(2, 5));
         }
-        if (i + 2 < Sentence.size()) {
-            score += ParsingModel.GetScore(Model::EParams::TW4, Sentence.substr(i, 3));
+        if (i + 2 < letters) {
+            score += ParsingModel.GetScore(Model::EParams::TW4, Cache.CreateView(3, 6));
         }
 
         if (score > 0) {
-            result.push_back(Sentence.substr(lastBound, i - lastBound));
-            lastBound = i;
+            result.push_back(std::string_view{lastBound, Cache[3]});
+            lastBound = Cache[3];
         }
+
+        Cache.ReadNext();
     }
 
-    result.push_back(Sentence.substr(lastBound));
+    if (lastBound != Sentence.cend()) {
+        result.push_back(std::string_view{lastBound, Sentence.cend()});
+    }
 
     return result;
 }
--- tests/source/parser_test.cpp
+++ tests/source/parser_test.cpp
@@ -23,9 +23,50 @@ TEST_CASE("Parser test") {
                     {{"a", 1000}},
                 }}};
 
-    const std::string_view TEST_SENTENCE = "abcdeabcd";
+    const std::string_view TEST_SENTENCE = "abcdeazxy";
     const Parser parser(model);
     const auto result = parser.Parse(TEST_SENTENCE);
     REQUIRE(result.size() == 2);
-    REQUIRE_THAT(result, Catch::Matchers::Equals(std::vector<std::string_view>{"abcde", "abcd"}));
+    REQUIRE_THAT(result, Catch::Matchers::Equals(std::vector<std::string_view>{"abcde", "azxy"}));
+}
+
+TEST_CASE("should separate even if it makes a phrase of one character") {
+    Model model{.params = {{
+                    {},
+                    {},
+                    {},
+                    {},
+                    {},
+                    {},
+                    {},
+                    {},
+                    {},
+                    {},
+                    {{"b", 1000}},
+                }}};
+    const std::string_view TEST_SENTENCE = "abcdeabcd";
+    const Parser parser(model);
+    const auto result = parser.Parse(TEST_SENTENCE);
+    REQUIRE(result.size() == 3);
+    REQUIRE_THAT(result, Catch::Matchers::Equals(std::vector<std::string_view>{"a", "bcdea", "bcd"}));
+}
+
+TEST_CASE("should return an empty list when the input is a blank string") {
+    const Parser parser({});
+    const auto result = parser.Parse("");
+    REQUIRE(result.size() == 0);
+}
+
+TEST_CASE("Parse with japanese model") {
+    Parser p{GetJAModel()};
+    std::string s = "Google の使命は、世界中の情報を整理し、世界中の人がアクセスできて使えるようにすることです。";
+    const auto result = p.Parse(s);
+    REQUIRE(result.size() == 11);
+    const std::vector<std::string_view> expected{"Google の", "使命は、",       "世界中の",     "情報を", "整理し、",  "世界中の",
+                                                 "人が",      "アクセスできて", "使えるように", "する",   "ことです。"};
+    REQUIRE_THAT(result, Catch::Matchers::Equals(expected));
+
+    const auto second = p.Parse("今日は天気です。");
+    const std::vector<std::string_view> expected_second{"今日は", "天気です。"};
+    REQUIRE_THAT(second, Catch::Matchers::Equals(expected_second));
 }
